0271eb61254085c0954d11524aa527bfe402a875
gabys4 <bibi.aquino4@gmail.com>
2022-09-13 18:00:02 -0300

    commit23

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
+import email
 from django.db import models
 
 # Create your models here.
+
+#============================================
+#    POR FAVOR SEM MIGRAÇOES POR AGORA 
+#============================================
+class Usuario (models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField()
+    senha = models.CharField(max_length=30)  #qual deveria ser o da senha? charfield?
+    senha2 = models.CharField(max_length=30)  #para comparar com a senha
+    # um IF para ver se as senhas são iguais
+
+class Questão (models.Model):
+    titulo = models.CharField(max_length=70)
+    # pergunta
+    # alternativas (tem que ser um numero x ou pode variar? se variar tem que descobrir como fazer)
+    # tipo (ex: completar a frase(cliclar no botão), marcar alternativa) (vai ter??)
+

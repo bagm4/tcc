6b2f1b0e3a4cbd575c0c5afd51e2c96c506462b6
gabys4 <bibi.aquino4@gmail.com>
2022-09-24 20:56:11 -0300

    commit27

--- main/forms.py
+++ main/forms.py
@@ -1,3 +1,4 @@
+from turtle import textinput
 from django import forms
 from .models import *
 
@@ -9,7 +10,15 @@ class CadastroForm(forms.ModelForm):
 
 
 class QuestaoForm(forms.ModelForm):
-   
+    error_css_class = 'error-field' 
+    required_css_class = 'forms_style'
+    #pergunta = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control", "style":"height : 150px;"}))
+    #alternativa1 = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    #alternativa2 = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    #alternativa3 = forms.CharField(widget=forms.TextInput(attrs={"class": "alternativa34"}))
+    #alternativa4 = forms.CharField(widget=forms.TextInput(attrs={"class": "alternativa34"}))
+    #certa = forms.CharField(widget=forms.TextInput(attrs={"style":"border-color : green;"}))
+
     class Meta:
         model = Questao
         fields = '__all__'

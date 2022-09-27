8b832241fcf6b269504541a91e7d083807f54a8f
gabys4 <bibi.aquino4@gmail.com>
2022-09-27 18:21:52 -0300

    commit30

---
+++ main/static/js/exemplo.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    var deleteBtn = $('.delete-btn');
+
+    $(deleteBtn).on('click', function(e) {
+
+        e.preventDefault(); /* ate aqui o botão de lixo esta travado */
+
+        var delLink = $(this).attr('href');
+        var result = confirm('Quer deletar esta tarefa?')
+
+        if(result) {
+            window.location.href = delLink
+        }
+    });
+
+});
